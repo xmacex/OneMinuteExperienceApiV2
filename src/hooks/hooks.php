3f00592b25cc6435d05402a317709e44ec5dcd5d
Mace Ojala <maco@itu.dk>
2019-10-21 19:04:38 +0200

    Conditionally start keeping the model updated when the image field is updated.

--- src/hooks/hooks.php
+++ src/hooks/hooks.php
@@ -9,6 +9,7 @@
 
 use \Directus\Application\Application;
 use \Directus\Services\FilesServices;
+use \Directus\Database\TableGatewayFactory;
 use \OneMinuteExperienceApiV2\AzureCustomVisionTrainer;
 
 require_once 'AzureCustomVisionTrainer.php';
@@ -41,6 +42,39 @@ return [
                 $config['prediction']['production_model']
             );
             $azure->doTheProductiveThings($image, $data);
-        }
+        },
+        'item.update.artwork' => function (array $artwork) {
+            $config = parse_ini_file('/var/www/1mev2/directus/config/ome.ini', true);
+
+            $container = Application::getInstance()->getContainer();
+            $logger = $container->get('logger');
+
+            $logger->debug('Artwork data', $artwork);
+
+            // The received item contains, beside the id and
+            // modification metadata, only the changed fields. So
+            // let's use that knowledge.
+            if (array_key_exists('image', $artwork)) {
+                // image was updated. Do stuff.
+                $azure = new AzureCustomVisionTrainer(
+                    $config['project']['endpoint'],
+                    $config['project']['id'],
+                    $config['training']['key'],
+                    $config['prediction']['resource_id'],
+                    $config['prediction']['production_model']
+                );
+
+                $filesService = new FilesServices($container);
+                $file = $filesService->findByIds($artwork['image']);
+                $image = $file['data'];
+
+                $logger->debug('Artwork image data', $image);
+
+                $azure->createImageFromUrls($image, $artwork);
+                // $azure->trainAndPublishIteration();
+            }
+            // TODO: Also if artist_name or title was updated, rename
+            // the tag.
+        },
     ]
 ];
